65e256d5cc171d0cc50c49baf91bc7d1f278bc75
redjax <jackenyon@gmail.com>
2022-10-22 13:41:47 -0400

    feat(example-fastapi): Update build_img script
    Fix variable name

--- nomad/job_files/containers/example-fastapi/build_img.sh
+++ nomad/job_files/containers/example-fastapi/build_img.sh
@@ -3,7 +3,7 @@
 image_name="example-fastapi"
 registry_address="localhost:5000"
 
-docker build . -t $container_name
+docker build . -t $image_name
 
 docker tag $image_name:latest $registry_address/$image_name
 docker push $registry_address/$image_name
